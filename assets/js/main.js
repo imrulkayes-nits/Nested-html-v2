0a90a777794f34eef4b18a3e8f30fdce65e41b4f
MD IMRUL KAYES <emimrulkayes@gmail.com>
2020-07-13 16:34:32 +0600

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+})
+
+const slidesContainer = document.querySelector(".slides-container");
+const slideImg = document.querySelectorAll(".slide-img");
+const nextBtn = document.querySelector (".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const navDots = document.querySelector(".nav-dots");
+const singleDot = document.querySelector(".single-dot");
+
+// Setup The Slider
+
+function init() {
+    
+
+    slideImg.forEach((img, i) => { 
+
+    }
+    )
+}
